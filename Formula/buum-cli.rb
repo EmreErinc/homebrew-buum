cae5c266a161f015bd53564367fa43cb054494c1
emre erinc <emreerinc@emre-macbook-air.taild76785.ts.net>
2026-03-09 23:47:53 +0300

    Bump buum-cli to v1.0.5

--- Formula/buum-cli.rb
+++ Formula/buum-cli.rb
@@ -1,9 +1,9 @@
 class BuumCli < Formula
   desc "Terminal tool to update Homebrew and Mac App Store apps"
   homepage "https://github.com/EmreErinc/buum-cli"
-  url "https://github.com/EmreErinc/buum-cli/releases/download/v1.0.4/buum-cli-1.0.4-universal.tar.gz"
-  sha256 "4c9daa8fb5f8d6af5338d76ee57fb3dcc0801e4c0f59f807a9276f1733aa4e7e"
-  version "1.0.4"
+  url "https://github.com/EmreErinc/buum-cli/releases/download/v1.0.5/buum-cli-1.0.5-universal.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "1.0.5"
   license "GPL-3.0-only"
 
   depends_on :macos
